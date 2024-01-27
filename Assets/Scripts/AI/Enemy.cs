fbfc0d7977dacb043011678c4b6de6279ddd33e7
Dominique Martins <dominique@lumen.games>
2024-01-27 18:50:09 -0300

    AI system done

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private CharacterController _character;
     [SerializeField] private EnemyStateMachine _enemyStateMachine;
+    [SerializeField] private NavMeshAgent _agent;
     private string _state;
     public bool gotHurt;
 
@@ -19,5 +22,19 @@ public class Enemy : MonoBehaviour
         {
             gotHurt = true;
         }
+    }
+
+    public float CheckPlayerDistance()
+    {
+        Vector3 point = _character.transform.position;
+
+        Vector3 target_direction = point - this.transform.position;
+        float target_distance = Vector3.Distance(this.transform.position, point);
+        return target_distance;
+    }
+
+    public void NavigateToPlayer()
+    {
+        _agent.SetDestination(_character.transform.position);
     }
 }
