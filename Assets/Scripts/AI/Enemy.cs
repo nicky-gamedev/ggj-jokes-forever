88b5557ad7986eed3e8ebd7ba0aec23c5de0d848
Dominique Martins <dominique@lumen.games>
2024-01-27 16:20:23 -0300

    Dummy enemy takes damage

---
+++ Assets/Scripts/AI/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemyStateMachine _enemyStateMachine;
+    private string _state;
+    public bool gotHurt;
+
+    private void Update()
+    {
+         _state = _enemyStateMachine.UpdateState(_state, this);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Projectile"))
+        {
+            gotHurt = true;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/EnemyStateMachine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyStateMachine", menuName = "Custom/StateMachine/Enemy", order = 0)]
+public class EnemyStateMachine : ScriptableObject
+{
+    private string previousState;
+    private float hurt_timeToLeave;
+    [SerializeField] private float hurt_cooldownTime;
+
+    public string UpdateState(string state, Enemy enemy)
+    {
+        if (state == "IDLE") return UpdateState_IDLE(enemy);
+        if (state == "HURT") return UpdateState_HURT(enemy);
+        return UpdateState_IDLE(enemy);
+    }
+
+    private string UpdateState_HURT(Enemy enemy)
+    {
+        if (Time.time >= hurt_timeToLeave)
+        {
+            return previousState;
+        }
+        return "HURT";
+    }
+
+    private string UpdateState_IDLE(Enemy enemy)
+    {
+        if (enemy.gotHurt)
+        {
+            hurt_timeToLeave = Time.time + hurt_cooldownTime;
+            previousState = "IDLE";
+            enemy.gotHurt = false;
+            return "HURT";
+        }
+        return "IDLE";
+    }
+}
