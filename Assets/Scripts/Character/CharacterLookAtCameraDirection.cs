3e934866f7f2bd4f55d5bc001528ab2faf3fd201
Eduardo Brandao <dudubrand@gmail.com>
2024-01-27 08:31:50 -0300

    Fixing player rotation to rotate to camera direction

---
+++ Assets/Scripts/Character/CharacterLookAtCameraDirection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterLookAtCameraDirection : MonoBehaviour
+{
+    [SerializeField] 
+    private Camera _cam;
+    
+    private void FixedUpdate()
+    {
+        ProcessLook();
+    }
+
+    private void ProcessLook()
+    {
+        transform.rotation = Quaternion.LookRotation(_cam.transform.forward, _cam.transform.up);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+}
