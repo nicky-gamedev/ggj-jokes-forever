6dcc271f97e0a678b15020880a513dd92e29e76a
Eduardo Brandao <dudubrand@gmail.com>
2024-01-27 11:41:57 -0300

    Added gravity to the player to fix the vertical movement

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(InputReader))]
 public class CharacterMovement : MonoBehaviour
 {
+    private readonly float GRAVITY_VALUE = -9.81f;
+    
     [SerializeField] 
     private InputReader _inputReader;
     [SerializeField] 
@@ -17,27 +19,24 @@ public class CharacterMovement : MonoBehaviour
     {
         _inputReader.Init();
         _inputReader.MovementEvent += ProcessMovement;
-        
     }
 
     private void OnDisable()
     {
         _inputReader.MovementEvent -= ProcessMovement;
     }
-
-    private void Start()
-    {
-        Cursor.visible = false;
-    }
-
+    
     private void ProcessMovement(Vector2 movementInput)
     {
         Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = movementInput.x;
-        moveDirection.z = movementInput.y;
-        moveDirection = _cam.transform.forward * moveDirection.z + _cam.transform.right * movementInput.x;
+        moveDirection = _cam.transform.forward * movementInput.y + _cam.transform.right * movementInput.x;
+        moveDirection.y = GRAVITY_VALUE/_speed;
         var motion = (moveDirection) * _speed * Time.deltaTime;
-        
         _controller.Move(motion);
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        Cursor.visible = false;
     }
 }
